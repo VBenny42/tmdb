fa5976fe471b56869c9f2a2b7237f0b157ecfe00
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2024-05-28 01:16:01 -0400

    changed episodes from a Grid to a List view, totally bypassing the need for tvshowepisode

--- src/components/Episodes.tsx
+++ src/components/Episodes.tsx
@@ -1,9 +1,8 @@
-import { ActionPanel, Action, Grid, Icon, showToast, Toast } from "@raycast/api";
+import { ActionPanel, Action, Grid, Icon, showToast, Toast, List, Color } from "@raycast/api";
 import { moviedb } from "../api";
 import { SimpleEpisode } from "moviedb-promise";
 import { useCachedPromise } from "@raycast/utils";
 import { useState } from "react";
-import TvShowEpisode from "./TvShowEpisode";
 
 function Episodes({
   id,
@@ -39,9 +38,6 @@ function Episodes({
     },
   });
 
-  const episodeStart = episodeData?.[0]?.episode_number || 0;
-  const episodeEnd = episodeData?.[episodeData.length - 1]?.episode_number || 0;
-
   const filteredEpisodes = ((episodeData as SimpleEpisode[]) || []).filter(
     (episode) =>
       selectedEpisode === "all" ||
@@ -50,76 +46,55 @@ function Episodes({
   );
 
   return (
-    <Grid
-      aspectRatio="16/9"
-      fit={Grid.Fit.Fill}
+    <List
       isLoading={isLoadingEpisodes || isLoadingInfo}
-      columns={3}
+      isShowingDetail
       searchBarPlaceholder={`Filter through ${showInfo} episodes by name`}
       navigationTitle={`TV Episodes - ${selectedEpisode === "all" ? "All" : selectedEpisode}`}
       searchBarAccessory={
-        <Grid.Dropdown tooltip="Filter by Episode" onChange={setSelectedEpisode} value={selectedEpisode}>
-          <Grid.Dropdown.Section title="Episodes">
-            <Grid.Dropdown.Item title="All" value="all" />
+        <List.Dropdown tooltip="Filter by Episode" onChange={setSelectedEpisode} value={selectedEpisode}>
+          <List.Dropdown.Section title="Episodes">
+            <List.Dropdown.Item title="All" value="all" />
             {(episodeData || []).map((episode) => (
-              <Grid.Dropdown.Item
+              <List.Dropdown.Item
                 key={episode.episode_number || "no-episode"}
                 title={episode.episode_number?.toString() || "Unknown Episode"}
                 value={episode.episode_number?.toString() || "no-episode"}
               />
             ))}
-          </Grid.Dropdown.Section>
-        </Grid.Dropdown>
+          </List.Dropdown.Section>
+        </List.Dropdown>
       }
     >
-      {filteredEpisodes.map((episode) => (
-        <Grid.Item
-          content={`https://image.tmdb.org/t/p/w342${episode.still_path}`}
-          title={`Episode ${episode.episode_number} - ${episode.name || "Unknown Episode"}`}
-          key={episode.still_path}
-          actions={
-            <ActionPanel>
-              <Action.Push
-                title="Show Episode Details"
-                icon={Icon.Info}
-                target={
-                  <TvShowEpisode
-                    showId={id}
-                    seasonNumber={seasonNumber}
-                    _episodeNumber={episode.episode_number ? episode.episode_number : 0}
-                    episodeStart={episodeStart}
-                    episodeEnd={episodeEnd}
+      {filteredEpisodes.map((episode) => {
+        const markdown = `![TV Show Banner](https://image.tmdb.org/t/p/w500${episode.still_path})\n\n${
+          episode.overview || ""
+        }\n\n**${episode.name}**`;
+        return (
+          <List.Item
+            title={`${episode.name || "Unknown Episode"}`}
+            key={episode.still_path}
+            accessories={[{ tag: { value: episode.episode_number?.toString() || "XX", color: "green" } }]}
+            detail={<List.Item.Detail markdown={markdown} />}
+            actions={
+              <ActionPanel>
+                <Action.OpenInBrowser
+                  title="Open in TMDB"
+                  url={`https://www.themoviedb.org/tv/${episode.show_id}/season/${episode.season_number}/episode/${episode.episode_number}`}
+                />
+                {episode.id ? (
+                  <Action.CopyToClipboard
+                    title={`Copy TMDB ID`}
+                    content={episode.id.toString()}
+                    shortcut={{ modifiers: ["cmd"], key: "i" }}
                   />
-                }
-              />
-              <Action.OpenInBrowser
-                url={`https://www.themoviedb.org/tv/${id}/season/${seasonNumber}/episode/${episode.episode_number}`}
-                title="Open in TMDB"
-              />
-              <Action.CopyToClipboard
-                content={`https://www.themoviedb.org/tv/${id}/season/${seasonNumber}/episode/${episode.episode_number}`}
-                title={`Copy TMDB URL`}
-                shortcut={{ modifiers: ["cmd"], key: "." }}
-              />
-              <Action
-                title="Next Season"
-                icon={Icon.ArrowRight}
-                onAction={() => {
-                  let setNumber = 0;
-                  if (_seasonNumber < numberOfSeasons) {
-                    setNumber = _seasonNumber + 1;
-                  }
-                  setSeasonNumber(setNumber);
-                  console.log("LOG: setNumber:", setNumber);
-                }}
-                shortcut={{ modifiers: ["cmd"], key: "arrowRight" }}
-              />
-            </ActionPanel>
-          }
-        />
-      ))}
-      {episodeData && filteredEpisodes.length === 0 && <Grid.Item title="No episodes available" content="" />}
-    </Grid>
+                ) : null}
+              </ActionPanel>
+            }
+          />
+        );
+      })}
+    </List>
   );
 }
 
